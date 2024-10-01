510810855c28a638f2f4097192454a39157548e7
belzDaniel <128147522+DanielBelz1997@users.noreply.github.com>
2024-10-01 14:39:41 +0000

    rf

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsStrongPassword } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsStrongPassword()
+    password: string;
+}
