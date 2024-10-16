8879e2b880c7cc3d8880c36824ea9bbf53bb604e
BelzDaniel <BelzDaniel6@gmail.com>
2024-10-16 16:54:28 +0300

    need to change the type of the impur from dto to prisma

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,11 +1,42 @@
-import { IsEmail, IsNotEmpty, IsStrongPassword } from "class-validator";
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  IsStrongPassword,
+  IsUrl,
+} from 'class-validator';
 
 export class CreateUserDto {
-    @IsNotEmpty()
-    @IsEmail()
-    email: string;
+  @IsNotEmpty()
+  @IsString()
+  first_name: string;
 
-    @IsNotEmpty()
-    @IsStrongPassword()
-    password: string;
+  @IsNotEmpty()
+  @IsString()
+  last_name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsStrongPassword()
+  password: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber()
+  phone_number: string;
+
+  @IsNotEmpty()
+  @IsString()
+  group: string;
+
+  @IsNotEmpty()
+  @IsUrl()
+  avatar: string;
 }
