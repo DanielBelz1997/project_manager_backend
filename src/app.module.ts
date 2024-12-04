b949088d161d05a2ede776778bfc21f41ef8404d
BelzDaniel <BelzDaniel6@gmail.com>
2024-12-04 17:24:42 +0200

    feat: generate stuff with nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProjectModule } from './project/project.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ComponentModule } from './component/component.module';
+import { GroupModule } from './group/group.module';
+import { PermissionModule } from './permission/permission.module';
+import { ContactModule } from './contact/contact.module';
+import { RequestModule } from './request/request.module';
 
 @Module({
-  imports: [ProjectModule, UserModule, AuthModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    ComponentModule,
+    GroupModule,
+    PermissionModule,
+    ContactModule,
+    RequestModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/contact/contact.service.spec.ts
+++ src/contact/contact.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProjectService } from './project.service';
+import { ContactService } from './contact.service';
 
-describe('ProjectService', () => {
-  let service: ProjectService;
+describe('ContactService', () => {
+  let service: ContactService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectService],
+      providers: [ContactService],
     }).compile();
 
-    service = module.get<ProjectService>(ProjectService);
+    service = module.get<ContactService>(ContactService);
   });
 
   it('should be defined', () => {
